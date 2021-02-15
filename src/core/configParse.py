5d4174e697b90d8c628265ea088d42feb6c5b6b8
Tarith <tarithj@gmail.com>
2021-02-15 20:35:23 +0530

    created a config parser

---
+++ src/core/configParse.py
@@ -0,0 +1,23 @@
+
+# parses and returns the values of the config [NOTE: config parser should be given only a line of text]
+
+
+def parse(config):
+    out = dict()
+
+    c = config.split(" ")
+    print(c)
+    out["minute"] = c[0]
+    out["hour"] = c[1]
+    out["date"] = c[2]
+    out["month"] = c[3]
+    out["year"] = c[4]
+
+    out["command"] = c[5]
+    i = 6
+    print(len(c))
+    while i < len(c):
+        out["command"] += " " + c[i]
+        i += 1
+
+    return out
